a4ba3940c74ddf5e1b1cf39b0b7508cd811ad5dd
abhijit <abhijitsahu300@gmail.com>
2023-09-23 20:13:48 +0530

    set up

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages,setup
+from typing import List;
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    requirement=[]
+    with open(file_path) as file_obj:
+        requirement=file_obj.readlines()
+        requirement=[req.replace("\n","")for req in requirement]
+        if HYPEN_E_DOT in requirement:
+            requirement.remove(HYPEN_E_DOT)
+
+setup(
+    name='mlproject',
+    version='0.0.1',
+    auther='Abhijit',
+    author_email='abhijitsahu300@gmail.com',
+    package=find_packages(),
+    install_requires=get_requirements('requirement.txt')
+
+)
