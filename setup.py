20b3656e5637fd62669ed76640983decb2a57fc2
abhijit <abhijitsahu300@gmail.com>
2023-09-24 20:41:57 +0530

    logging exception

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name='mlproject',
     version='0.0.1',
-    auther='Abhijit',
+    author='Abhijit',
     author_email='abhijitsahu300@gmail.com',
     package=find_packages(),
     install_requires=get_requirements('requirement.txt')
